05c1f1910907b7844adec0e928ac3e0e3abc6ea7
Unclephil <philipnssien@gmail.com>
2023-12-20 15:15:43 +0100

    added Id

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  id: { type: mongoose.Schema.Types.ObjectId, required: true, default: mongoose.Types.ObjectId },
   name: { type: String, required: true },
   sector: { type: String, required: true },
   agreeToTerms: { type: Boolean, required: true },
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
+// routes/api.js
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 
 const router = express.Router();
 
+// POST route for saving or updating a user
 router.post(
   '/save-user',
   [
@@ -18,14 +20,37 @@ router.post(
         return res.status(400).json({ errors: errors.array() });
       }
 
+      // Check if the user already exists
+      const existingUser = await User.findOne({ name: req.body.name });
+
+      if (existingUser) {
+        // Update the existing user
+        existingUser.sector = req.body.sector;
+        existingUser.agreeToTerms = req.body.agreeToTerms;
+        const updatedUser = await existingUser.save();
+        return res.json({ success: true, user: updatedUser });
+      }
+
+      // Create a new user
       const newUser = new User(req.body);
       const savedUser = await newUser.save();
-      res.json(savedUser);
+      res.json({ success: true, user: savedUser });
     } catch (error) {
-      console.error('Error saving user:', error);
+      console.error('Error saving or updating user:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   }
 );
+
+// GET route to fetch all users
+router.get('/get-user', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;
